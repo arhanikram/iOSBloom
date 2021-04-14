e1ce45f2dfe469df0cbdbd4242e792445d041359
Arhan Ikram <arhan.ikram@gmail.com>
2021-04-14 18:10:00 -0400

    Added tableView named plantList for the list of plants

--- Bloom/ViewController.swift
+++ Bloom/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var calendar: FSCalendar!
-    //@IBOutlet weak var calendar: FSCalendar!
+
+    @IBOutlet weak var plantList: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.\
+        
+        
     }
+    
 
 
 }
