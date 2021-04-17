9c0b1c8e9749c875c8f1d8a06920fa4e541e4782
Arhan Ikram <arhan.ikram@gmail.com>
2021-04-16 20:31:36 -0400

    Table view (plantList) now displays first plant in garden

--- Bloom/ViewController.swift
+++ Bloom/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+
 
     
     @IBOutlet weak var plantNameText: UITextField!
@@ -25,18 +27,41 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var calendar: FSCalendar!
 
-    @IBOutlet weak var plantList: UITableView!
+    @IBOutlet weak var plantList: UITableView!/*{
+        didSet{
+            plantList.dataSource = self
+            plantList.delegate = self
+        }
+    }*/
+    
+    let cellReuseIdentifier = "cell"
+    
+    var plants: [String] = []
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         newGarden = Garden()
+        
+        if plantList != nil{
+            
+            self.plantList.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        
         // Do any additional setup after loading the view.\
         
-
         
+        plantList.delegate = self
+        plantList.dataSource = self
         
-    
+        //plantList.estimatedRowHeight = 200.0
+        plantList.rowHeight = UITableView.automaticDimension
+        
+        plants.append(newGarden.currentFlower().printFlower())
+        
+        }
     
     /*func calendar( _ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition){
             let formatter = DateFormatter()
@@ -46,6 +71,46 @@ class ViewController: UIViewController {
         }*/
         
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.plants.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = (self.plantList.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.plants[indexPath.row]
+        
+        cell.sizeToFit()
+        cell.textLabel?.sizeToFit()
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = .byWordWrapping
+
+        
+        return cell
+    }
+    
+
+
+    
+/*    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newGarden.currentIndex()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = plantList.dequeueReusableCell(withIdentifier: "TableViewCell")!
+        cell.textLabel?.text = "test"
+        return cell
+    }*/
+
 
 
     @IBAction func addButton(_ sender: Any) {
@@ -55,15 +120,22 @@ class ViewController: UIViewController {
         
         //I was thinking this button could take us to the new plant view screen as well maybe
 
-        newFlower = Flower(flowerName: plantNameText.text!, lastWatered: lastWateredText.text!, lastFertilized: lastFertilizedText.text!, waterCylce: Int(waterCycleText.text!)!, fertilizerCycle: Int(fertilizerCycleText.text!)!)
+        if !(plantNameText.text!.isEmpty) && !(lastWateredText.text!.isEmpty) && !(lastFertilizedText.text!.isEmpty) && !(waterCycleText.text!.isEmpty) && !(fertilizerCycleText.text!.isEmpty) {
+        
+            
+            newFlower = Flower(flowerName: plantNameText.text!, lastWatered: lastWateredText.text!, lastFertilized: lastFertilizedText.text!, waterCylce: Int(waterCycleText.text!)!, fertilizerCycle: Int(fertilizerCycleText.text!)!)
         
         newGarden.addFlower(flowerObj: newFlower)
 
         
         newGarden.printGarden()
+        }
+        else{
+            print("All values must be filled")
+        }
         
         
-
     }
+
 }
 
